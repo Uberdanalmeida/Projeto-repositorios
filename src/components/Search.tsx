2ba954c71ef90c6c663192283d7a49f5ec4b9694
Uberdanalmeida <105062084+Uberdanalmeida@users.noreply.github.com>
2025-08-01 12:28:04 -0300

    Criando uma função para quando der "ENTER" no botão.

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,7 +2,7 @@ type SearchProps = {
     loadUser: (userName: string) => Promise<void>
 }
 
-import { useState } from "react"
+import { useState, type KeyboardEvent} from "react"
 import { BsSearch } from "react-icons/bs"
 import classes from './Search.module.css'
 
@@ -10,12 +10,18 @@ function Search({loadUser}: SearchProps) {
 
     const [userName, setUserName] = useState("")
 
+    const handleKeyDown = (e:KeyboardEvent) => {
+        if(e.key === "Enter") {
+            loadUser(userName)
+        }
+    }
+
     return(
         <div className={classes.search}>
             <h2>Busque por um usuário</h2>
             <p>Conheça seus Repositorios</p>
             <div className={classes.search_container}>
-            <input type="text" name="" id="" placeholder="Busque por um usuário" onChange={(e) => setUserName(e.target.value)}/>
+            <input type="text" name="" id="" placeholder="Busque por um usuário" onChange={(e) => setUserName(e.target.value)} onKeyDown = {handleKeyDown}/>
             <button onClick={() => loadUser(userName)}><BsSearch></BsSearch></button>
             </div>
         </div>
